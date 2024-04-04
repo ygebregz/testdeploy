1a0ceed3332c1000ed801eb15342585e325b3c8b
Yonas Gebregziabher <ygebregz@bowdoin.edu>
2024-04-03 23:48:47 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+from flask import Flask, request
+from predict import make_prediction
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    """Basic HTML response."""
+    body = (
+        "<html>"
+        "<body style='padding: 10px;'>"
+        "<h1>Welcome to my Flask API</h1>"
+        "</body>"
+        "</html>"
+    )
+    return body
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    print("files", request.files)
+    if 'image' not in request.files:
+        return 'No file part in the request', 400
+    
+    file = request.files['image']
+    
+    if file.filename == '':
+        return 'No selected file.', 400
+    
+    pred = make_prediction(file)
+    
+  
+    return pred, 200
+
+if __name__ == "__main__":
+    app.run()
---
+++ predict.py
@@ -0,0 +1,26 @@
+import joblib
+import pandas as pd
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import load_img, img_to_array
+import numpy as np
+from PIL import Image
+
+model = load_model('my_model.h5')
+
+def make_prediction(image):
+    """
+    Make a prediction using the trained model
+    """
+    img = Image.open(image).convert('RGB').resize((300,300))
+    x = img_to_array(img)
+    x /= 255 #normalize
+    x = np.expand_dims(x, axis=0)
+    
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size =10)
+    
+    print(classes)
+    if classes[0] > 0.5:
+        return "it is a human"
+    return "it is a horse"
+

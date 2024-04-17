11e9484ad9e7f8db991ed729e2cb0b2eb9c01d57
Yonas Gebregziabher <ygebregz@bowdoin.edu>
2024-04-17 10:20:35 -0400

    working

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-import numpy as np
-from flask import Flask, request, render_template, url_for, jsonify
-from flask_cors import CORS, cross_origin
-import logging
-from predict import make_prediction
+from flask import Flask, request, render_template
 
-logging.basicConfig(level=logging.DEBUG)
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import img_to_array
+import numpy as np
+from PIL import Image
+
+
+#TODO : get from bucket / drive /
+model = load_model('my_model.h5')
+
+def make_prediction(image):
+    """
+    Make a prediction using the trained model
+    """
+    img = Image.open(image).convert('RGB').resize((300,300))
+    x = img_to_array(img)
+    x /= 255 #normalize
+    x = np.expand_dims(x, axis=0)
+    
+    images = np.vstack([x])
+    
+    classes = model.predict(images, batch_size =10)
+    if classes[0] > 0.5:
+        return "Human"
+    return "Horse"
+
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
@@ -14,21 +35,15 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    logging.debug("got into predict api endpoint")
     print("files", request.files)
     if 'image' not in request.files:
-        logging.debug("image not in request file")
         return 'No file part in the request', 400
     
     file = request.files['image']
-    logging.debug("got into file")
     if file.filename == '':
         return 'No selected file.', 400
     
-    pred = make_prediction(file)
-    logging.debug("returning result from endpoint")
-    
-  
+    pred = make_prediction(file)  
     return render_template("results.html", prediction = pred, comment="asd")
 
 if __name__ == '__main__':
